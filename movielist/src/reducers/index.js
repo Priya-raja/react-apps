9fb7c93dd6584eae24a716e9b7ee4ff1f2bc1f83
Priya-raja <priya.thevar89@gmail.com>
2021-02-13 13:39:20 +0530

    reducer function

--- movielist/src/reducers/index.js
+++ movielist/src/reducers/index.js
@@ -1,6 +1,24 @@
-const movieReducer = () => {
-    return [
-        {title:'Spider-man: Homecoming', releaseDate: '05-05-2017', rating:7.4, }
-    ]
+import { combineReducers } from 'redux';
 
+const moviesReducer = () => {
+    return [
+        {title:'Spider-man: Homecoming', releaseDate: '05-05-2017', rating:7.4, },
+        {title: 'Avengers-Infinity', releaseDate: '09-08-2014', rating:4.5, },
+        {title:'Jurassic Park', releaseDate: '05-04-2016', rating:6.4, },
+        {title: 'Black Panther', releaseDate: '12-06-2018', rating:7.5, },
+    ]
 }
+
+const selectedMovieReducer = (state = null, action) => {
+    switch(action.type) {
+        case 'MOVIE_SELECTED':
+            return action.payload
+        default:
+            return state
+    }
+}
+
+export default combineReducers({
+    movies: moviesReducer,
+    selectedMovie: selectedMovieReducer
+})
