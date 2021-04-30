538be7f60b6d21ab370b98bb6aba12f6f33324c1
Priya-raja <priya.thevar89@gmail.com>
2021-04-30 23:14:22 +0530

    Initiated a tic tac toe assignment

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Icon from './components/Icon';
+import './App.css';
+
+function App() {
+  return(
+    <div className="App">
+       <Icon />
+      <h1> Game of tic tac toe</h1>
+     
+    </div>
+
+  );
+  
+  
+}
+
+export default App;
---
+++ tic-tac-toe/src/components/Icon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {FaTimes, FaPen, FaRegCircle} from 'react-icons/fa'
+const Icon = ({name}) => {
+    
+            switch (name) {
+                case "Circle":
+                    return <FaRegCircle className="icons />
+
+
+            }
+
+        
+}
+
+export default Icon
