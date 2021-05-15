1e9f5de85fad6af1a6cbbb1056a34eb5b3d17968
Priya-raja <priya.thevar89@gmail.com>
2021-05-15 22:45:03 +0400

    Added store to index and actions

--- redux-banking/src/index.js
+++ redux-banking/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import {createStore} from 'redux'
-import {bankingReducer} from './reducers/bankingReducer'
+import { createStore } from 'redux'
+import { bankingReducer } from './reducers/bankingReducer'
+import { Provider } from 'react-redux';
 
 const store = createStore(bankingReducer);
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App/>
+    </Provider>,
+
+document.getElementById('root'));
 
 
--- redux-banking/src/reducers/bankingReducer.js
+++ redux-banking/src/reducers/bankingReducer.js
@@ -1,4 +1,27 @@
+
 export const bankingReducer = (state, action) => {
+    switch(action.type) {
+        case "DEPOSIT" :
+            return state + action.amount;
+        
+        case "WITHDRAW":
+            return state - action.amount;
+
+        default:
+            return state;    
+
+
+    }
 
     return state // returns old state
+}
+
+const deposit = {
+    type: "DEPOSIT",
+    amount: 20
+}
+
+const withdraw = {
+    type: "WITHDRAW",
+    amount: 3
 }
